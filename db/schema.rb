d94779254ba8a59c552b56e27bb388f286bf8409
Candace Eckels <candace.eckels@gmail.com>
2023-01-03 17:13:29 -0700

    Added subscriptions model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_03_235643) do
+ActiveRecord::Schema.define(version: 2023_01_04_001017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.string "title"
+    t.integer "price"
+    t.integer "status"
+    t.integer "frequency"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "teas", force: :cascade do |t|
     t.string "title"
