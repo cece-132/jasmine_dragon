c69a8dba119f37372e7f18fb09ea0e36660421d3
Candace Eckels <candace.eckels@gmail.com>
2023-01-03 17:44:33 -0700

    Updated customer relationships

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe Customer, type: :model do
   end
 
   describe 'relationships' do
-    
+    it { should have_many(:customer_subscriptions) }
+    it { should have_many(:subscriptions).through(:customer_subscriptions) }
   end
 end
