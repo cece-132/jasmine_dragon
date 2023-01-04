31a20e5c489dcee96ae76facbb2acc9149bbfca0
Candace Eckels <candace.eckels@gmail.com>
2023-01-04 11:17:46 -0700

    Updated cus_subs validations

--- spec/models/customer_subscription_spec.rb
+++ spec/models/customer_subscription_spec.rb
@@ -4,6 +4,9 @@ RSpec.describe CustomerSubscription, type: :model do
   describe 'validations' do
     it { should validate_presence_of :customer_id }
     it { should validate_presence_of :subscription_id }
+    it { should validate_presence_of :status }
+    it { should define_enum_for(:status).with_values(["Active", "Cancelled"]) }
+
   end
 
   describe 'relationships' do
