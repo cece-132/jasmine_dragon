d698c742fab1bd906e31f79e949251e2b94bf4a8
Candace Eckels <candace.eckels@gmail.com>
2023-01-03 17:38:25 -0700

    Added customer_subscription model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_04_001628) do
+ActiveRecord::Schema.define(version: 2023_01_04_003355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "customer_subscriptions", force: :cascade do |t|
+    t.bigint "customer_id"
+    t.bigint "subscription_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_customer_subscriptions_on_customer_id"
+    t.index ["subscription_id"], name: "index_customer_subscriptions_on_subscription_id"
+  end
 
   create_table "customers", force: :cascade do |t|
     t.string "first_name"
@@ -42,4 +51,6 @@ ActiveRecord::Schema.define(version: 2023_01_04_001628) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "customer_subscriptions", "customers"
+  add_foreign_key "customer_subscriptions", "subscriptions"
 end
