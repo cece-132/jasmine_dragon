1b5adada11f7146af3e47dc5903db6207e1681e6
Candace Eckels <candace.eckels@gmail.com>
2023-01-03 18:21:51 -0700

    Added customer subscription request file

---
+++ spec/api/v1/requests/customer_subscriptions_request.rb
@@ -0,0 +1,8 @@
+# Create a customer subscription
+  # An endpoint to subscribe a customer to a tea subscription
+
+# Destroy a customer subscription
+  # An endpoint to cancel a customer’s tea subscription
+
+# Read all the customers subscriptions
+  # An endpoint to see all of a customer’s subsciptions (active and cancelled)
